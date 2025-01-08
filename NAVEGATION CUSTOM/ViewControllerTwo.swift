efce83b5cf786808905c9f05da12324c019518bc
jairrgz27 <127373009+jairrgz@users.noreply.github.com>
2025-01-08 11:13:30 -0500

    Added all the type of presentations and Transitions to test

--- NAVEGATION CUSTOM/ViewControllerTwo.swift
+++ NAVEGATION CUSTOM/ViewControllerTwo.swift
@@ -30,20 +30,6 @@ class ViewControllerTwo: UIViewController {
         
         // Crear botones para diferentes estilos
         let presentations = [
-//            PresentationStyle     
-//            case formSheet
-//            case fullScreen
-//            case overCurrentContext
-//            case overFullScreen
-//            case pageSheet
-            
-//            Transition
-//            case coverVertical
-//            case flipHorizontal
-//            case crossDissolve
-//            case partialCurl
-            
-        //CrossDisolve an PartialCurl solo hacen hasta cierta parte el modal y flipHorizontal and CovertVertical cubren todo
             ("Automatic - With CovertVertial", PresentationStyle.automatic, TransitionStyle.coverVertical),
             ("Automatic - With CrosDissolve", PresentationStyle.automatic, TransitionStyle.crossDissolve),
             ("Automatic - With FlipHorizontal", PresentationStyle.automatic, TransitionStyle.flipHorizontal),
@@ -54,13 +40,30 @@ class ViewControllerTwo: UIViewController {
             ("CurrentContext - With FlipHorizontal", PresentationStyle.currentContext, TransitionStyle.flipHorizontal),
             ("CurrentContext - With PartialCurl", PresentationStyle.currentContext, TransitionStyle.partialCurl), // Tampoco funciona el partialCurl
             
+            ("FormSheet - With CovertVertial", PresentationStyle.formSheet, TransitionStyle.coverVertical),
+            ("FormSheet - With CrosDissolve", PresentationStyle.formSheet, TransitionStyle.crossDissolve),
+            ("FormSheet - With FlipHorizontal", PresentationStyle.formSheet, TransitionStyle.flipHorizontal),
+            ("FormSheet - With PartialCurl", PresentationStyle.formSheet, TransitionStyle.partialCurl), // Tampoco funciona el partialCurl
             
+            ("FullScreen - With CovertVertial", PresentationStyle.fullScreen, TransitionStyle.coverVertical),
+            ("FullScreen - With CrosDissolve", PresentationStyle.fullScreen, TransitionStyle.crossDissolve),
+            ("FullScreen - With FlipHorizontal", PresentationStyle.fullScreen, TransitionStyle.flipHorizontal),
+            ("FullScreen - With PartialCurl", PresentationStyle.fullScreen, TransitionStyle.partialCurl), // Tampoco funciona el partialCurl
             
+            ("OverCurrentContext - With CovertVertial", PresentationStyle.overCurrentContext, TransitionStyle.coverVertical),
+            ("OverCurrentContext - With CrosDissolve", PresentationStyle.overCurrentContext, TransitionStyle.crossDissolve),
+            ("OverCurrentContext - With FlipHorizontal", PresentationStyle.overCurrentContext, TransitionStyle.flipHorizontal),
+            ("OverCurrentContext - With PartialCurl", PresentationStyle.overCurrentContext, TransitionStyle.partialCurl), // Tampoco funciona el partialCurl
             
+            ("OverFullScreen - With CovertVertial", PresentationStyle.overFullScreen, TransitionStyle.coverVertical),
+            ("OverFullScreen - With CrosDissolve", PresentationStyle.overFullScreen, TransitionStyle.crossDissolve),
+            ("OverFullScreen - With FlipHorizontal", PresentationStyle.overFullScreen, TransitionStyle.flipHorizontal),
+            ("OverFullScreen - With PartialCurl", PresentationStyle.overFullScreen, TransitionStyle.partialCurl), // Tampoco funciona el partialCurl
             
-            ("Page Sheet", PresentationStyle.pageSheet, TransitionStyle.crossDissolve),
-            ("Over FullScreen", PresentationStyle.overFullScreen, TransitionStyle.flipHorizontal),
-            ("Form Sheet", PresentationStyle.formSheet, TransitionStyle.flipHorizontal)
+            ("PageSheet - With CovertVertial", PresentationStyle.pageSheet, TransitionStyle.coverVertical),
+            ("PageSheet - With CrosDissolve", PresentationStyle.pageSheet, TransitionStyle.crossDissolve),
+            ("PageSheet - With FlipHorizontal", PresentationStyle.pageSheet, TransitionStyle.flipHorizontal),
+            ("PageSheet - With PartialCurl", PresentationStyle.pageSheet, TransitionStyle.partialCurl)
         ]
         
         for (title, presentation, transition) in presentations {
@@ -90,10 +93,30 @@ class ViewControllerTwo: UIViewController {
             (PresentationStyle.currentContext, TransitionStyle.flipHorizontal),
             (PresentationStyle.currentContext, TransitionStyle.partialCurl),
             
+            (PresentationStyle.formSheet, TransitionStyle.coverVertical),
+            (PresentationStyle.formSheet, TransitionStyle.crossDissolve),
+            (PresentationStyle.formSheet, TransitionStyle.flipHorizontal),
+            (PresentationStyle.formSheet, TransitionStyle.partialCurl),
             
-            (PresentationStyle.pageSheet, TransitionStyle.crossDissolve),
+            (PresentationStyle.fullScreen, TransitionStyle.coverVertical),
+            (PresentationStyle.fullScreen, TransitionStyle.crossDissolve),
+            (PresentationStyle.fullScreen, TransitionStyle.flipHorizontal),
+            (PresentationStyle.fullScreen, TransitionStyle.partialCurl),
+            
+            (PresentationStyle.overCurrentContext, TransitionStyle.coverVertical),
+            (PresentationStyle.overCurrentContext, TransitionStyle.crossDissolve),
+            (PresentationStyle.overCurrentContext, TransitionStyle.flipHorizontal),
+            (PresentationStyle.overCurrentContext, TransitionStyle.partialCurl), // Tampoco funciona el partialCurl
+            
+            (PresentationStyle.overFullScreen, TransitionStyle.coverVertical),
+            (PresentationStyle.overFullScreen, TransitionStyle.crossDissolve),
             (PresentationStyle.overFullScreen, TransitionStyle.flipHorizontal),
-            (PresentationStyle.formSheet, TransitionStyle.flipHorizontal)
+            (PresentationStyle.overFullScreen, TransitionStyle.partialCurl), // Tampoco funciona el partialCurl
+            
+            (PresentationStyle.pageSheet, TransitionStyle.coverVertical),
+            (PresentationStyle.pageSheet, TransitionStyle.crossDissolve),
+            (PresentationStyle.pageSheet, TransitionStyle.flipHorizontal),
+            (PresentationStyle.pageSheet, TransitionStyle.partialCurl)
         ]
         
         let (presentationStyle, transitionStyle) = presentations[sender.tag]
